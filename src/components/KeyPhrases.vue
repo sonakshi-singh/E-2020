c929c51d3836647bc509ac7d1a0a11da5b5a099e
Nick Gulson <nicholasgulson2021@u.northwestern.edu>
2020-02-18 21:08:23 -0600

    Forgot File

---
+++ src/components/KeyPhrases.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="phraseBox">
+    <h3 align="left">Key Phrases in Japanese</h3>
+    <div v-bind:key="phrase.id" v-for="phrase in phrases">
+      <p align="left">
+      {{phrase.eng_phrase}} -> {{phrase.trans_phrase}}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'KeyPhrases',
+    props: ["phrases"],
+    data(){
+        return {
+            phrase:"huh"
+        }
+    }
+}
+</script>
