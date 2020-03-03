c344249da7c30690c323a2b4ae604adec480941f
Nick Gulson <nicholasgulson2021@u.northwestern.edu>
2020-03-03 01:52:14 -0600

    Forgot one

---
+++ src/components/Vaccinations.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h2 align="left"><b>Vaccinations recommended for travel</b></h2>
+    <div v-bind:key="vac.id" v-for="vac in vaccination">
+      <p align="left">
+       <b> 
+      <input type="checkbox" align="left">
+        {{vac.vacc}}
+      </b>
+      </p>
+    </div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Vaccinations',
+  props: ["vaccination"]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.is-Complete{
+  text-decoration:line-through;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
