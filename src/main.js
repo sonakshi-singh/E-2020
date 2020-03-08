aa0256bfc15d121ddd5eceb936bcf85b5cedb875
soniatocode <sonakshi_singh@hotmail.com>
2020-03-08 14:25:55 -0500

    conflict

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { BootstrapVue } from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router';
