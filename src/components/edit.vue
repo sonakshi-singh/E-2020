294a68169cffaa33726d7e8d4117f2fa7d8b2299
soniatocode <sonakshi_singh@hotmail.com>
2020-06-07 20:22:45 -0500

    made some changes

--- src/components/edit.vue
+++ src/components/edit.vue
@@ -1,55 +1,27 @@
-<template >
+ <template >
   <div id="edit-container">
-    <!-- <div style="background-image: url(./src/body.jpg);"> -->
-    <!-- <img alt="Vue logo" src="./body.jpg"> -->
     <h1 style="font-size:60px;">Fill out Template</h1>
+    
     <div>
-            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            
      <label>Title</label>
-      <b-form-group
-        id="input-group-1"
-        label="Title"
-        label-for="input-1"
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.title"
-          required
-          placeholder="Enter title"
-        ></b-form-input>
-      </b-form-group>
-
-<label for="example-input">Choose a date</label>
-    <b-form-group id="input-group-2" label="Date" label-for="input-2">
-      <b-form-input
-        id="input-group-2"
-        v-model="value"
-        type="text"
-        placeholder="YYYY-MM-DD"
-        autocomplete="off"
-      ></b-form-input>
-      <b-form-group-append>
-        <b-form-datepicker
-          v-model="value"
-          button-only
-          right
-          locale="en-US"
-          aria-controls="example-input"
-          @context="onContext"
-        ></b-form-datepicker>
-      </b-form-group-append>
-    </b-form-group>
+      <b-form-input id="input-1" v-model="title" required placeholder="Enter title"></b-form-input>
+      <label for="example-input">Choose a date</label>
+      <b-form-datepicker id="example-datepicker" v-model="value" class="mb-2"></b-form-datepicker>
+       <b-form-textarea id="textarea" v-model="text" placeholder="Enter something..." rows="3" max-rows="6"></b-form-textarea>
+       <b-form-textarea id="textarea1" v-model="text1" placeholder="Enter something..." rows="3" max-rows="6"></b-form-textarea>
 
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-  </b-form>
+      <b-button type="submit" variant="primary">Preview</b-button>
+  
 
 
 
 
     </div>
+  </div>
    
-    </div>
+    
 </template>
 
 <script>
@@ -58,55 +30,40 @@
    name:"edit",
     data() {
       return {
-        form: {
-          title: '',
-          name:'',
-          value:''
+         
+          'title':'',
+          'name':'',
+          'value':'',
+          'text':'',
+          'text1:''
          
          
-        },
+        }
        
-      }
-    },
-    methods: {
-      onSubmit(evt) {
-        evt.preventDefault()
-        alert(JSON.stringify(this.form))
-      },
-      onReset(evt) {
-        evt.preventDefault()
-        // Reset our form values
-       
-        this.form.name = ''
-        
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
       }
     }
-  }
+  
 
 
 </script>
 
 <style >
-.ret{
-    width: 300px;
-    height :500px;
-    padding:20px;
-}
 
-#edit-container { 
-  /* background-image: url("./body.jpg");
-  background-size: length; */
-  /* background-color: darkslategray; */
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color:black;
-  margin-top: 60px;
+#input-1{
+
+width: 50%;
+margin-left: 25%;
+margin-right:5%;
+}
+.b-form-datepicker
+{
+width: 50%;
+margin-left: 25%;
+margin-right:5%;
+
 }
 
 </style>
+
+
+
