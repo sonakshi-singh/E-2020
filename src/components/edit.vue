ecaca28402f4bbddf496d200cc3424046cd5d35e
soniatocode <sonakshi_singh@hotmail.com>
2020-06-07 21:58:43 -0500

    made changes to edit template page

--- src/components/edit.vue
+++ src/components/edit.vue
@@ -1,15 +1,14 @@
  <template >
   <div id="edit-container">
-    <h1 style="font-size:60px;">Fill out Template</h1>
+    <h1 style="font-size:40px;margin-top:5%">Fill out Template</h1>
     
     <div>
             
-     <label>Title</label>
+    
       <b-form-input id="input-1" v-model="title" required placeholder="Enter title"></b-form-input>
-      <label for="example-input">Choose a date</label>
       <b-form-datepicker id="example-datepicker" v-model="value" class="mb-2"></b-form-datepicker>
-       <b-form-textarea id="textarea" v-model="text" placeholder="Enter something..." rows="3" max-rows="6"></b-form-textarea>
-       <b-form-textarea id="textarea1" v-model="text1" placeholder="Enter something..." rows="3" max-rows="6"></b-form-textarea>
+       <b-form-textarea id="textarea" v-model="text" placeholder="What happened?" rows="3" max-rows="6"></b-form-textarea>
+       <b-form-textarea id="textarea1" v-model="text1" placeholder="Top 3 things" rows="3" max-rows="6"></b-form-textarea>
 
 
       <b-button type="submit" variant="primary">Preview</b-button>
@@ -35,7 +34,7 @@
           'name':'',
           'value':'',
           'text':'',
-          'text1:''
+          'text1':''
          
          
         }
@@ -54,15 +53,30 @@
 width: 50%;
 margin-left: 25%;
 margin-right:5%;
+margin-bottom:2%;
 }
 .b-form-datepicker
 {
 width: 50%;
 margin-left: 25%;
 margin-right:5%;
+padding-bottom:5%;
 
 }
-
+#textarea
+{ 
+  width:50%;
+  margin-left:25%;
+  margin-right:5%;
+  margin-bottom:2%;
+}
+#textarea1
+{
+  width:50%;
+  margin-left:25%;
+  margin-right:5%;
+  margin-bottom:2%;
+}
 </style>
 
 
