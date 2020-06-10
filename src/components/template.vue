5aa3014cbfdb695483417237bc06a90015d8b4bf
Nick Gulson <nicholasgulson2021@u.northwestern.edu>
2020-06-09 19:38:10 -0500

    stuff i

--- src/components/template.vue
+++ src/components/template.vue
@@ -1,22 +1,32 @@
 <template>
   <div>
+   <h1 style="font-size:40px;margin-top:5%">View Template</h1>
     <b-container fluid="" class="template-container" >
       <br>
-      <br><br><br><br><br><br>
-
-      <img src= this.images[0] >
-      <input  :value="this.messages[0]" placeholder ="text" @change="messageChange" >
-      <img src= this.images[1] >
-      <input  :value="this.messages[1]" placeholder ="text" >
-    </b-container>
+      <br><br><br><br><br><br><br><br><br><br>
+           <input  :value="this.form.title" class = "title" placeholder ="text" >
+          <b-row align-h="center">
+            <b-col cols="4">
+            <img :src="this.form.image1" class="ret"  >
+            </b-col><b-col cols="4">
+            <input  :value="this.form.text" class = "txt" placeholder ="text" @change="messageChange" >
+            </b-col>
+            </b-row >
+          <b-row align-h="center">
+            <b-col cols="4">
+               <input  :value="this.form.text1" class = "txt" placeholder ="text" >
+            </b-col><b-col cols="4">
+              <img :src="this.form.image2" class="ret"  >
+          </b-col></b-row>
+          </b-container>
   </div>
 </template>
 
 <script>
-import getImageList from "../awsCalls/getImageList.js"
 
 export default {
-    name:"template",
+    name:"Template",
+    props: ["form"],
     data(){
         return {
             title: "title",
@@ -29,13 +39,7 @@ export default {
         console.log(event)
         console.log(this.images)
       }
-    },
-    components: {
-    },
-    created: function() {
-      this.images = getImageList(["NickGulson/MikeGrad1.jpeg","NickGulson/MikeGrad2.jpeg"])
-      console.log(this.images)
-  }
+    }
 };
 </script>
 
