0211be67e8022fdddc3ce4ed1fa9ed338b0b6124
aakanksha1 <aakanksha720@gmail.com>
2020-06-09 19:17:19 -0500

    added files back

---
+++ src/awsCalls/getImageList.js
@@ -0,0 +1,7 @@
+function getImageURL(username, filename){
+    return "https://beneimages.s3.us-east-2.amazonaws.com/" + username + "/" + filename
+}
+
+export default getImageURL
+
+//this should catch
